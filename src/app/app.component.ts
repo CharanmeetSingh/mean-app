c8b5890090b5c44a87912c0b7b806be91e65fd1d
Charanmeet Singh <charanmeet95@gmail.com>
2021-09-08 10:07:44 +1000

    migration to macbook

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Post } from '../app/posts/post.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  storedPosts: Post[] = [];
+  
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
+}
